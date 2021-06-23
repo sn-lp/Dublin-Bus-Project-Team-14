af1bd5a20d8d0c25ab005c9d3d8da858db6916f2
Sofia Pereira <66690399+sn-lp@users.noreply.github.com>
2021-06-23 09:28:52 +0100

    Add views following desktop mockups (#2)

--- dublinbus/main/urls.py
+++ dublinbus/main/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("realtime/", views.real_time, name="realtime"),
+    path("busroutes/", views.bus_routes, name="busroutes"),
 ]
