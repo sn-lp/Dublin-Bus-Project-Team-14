e0ca1fef82c851c62e9d0a313e7c534a78a67d7d
AY <anyi.huang@ucdconnect.ie>
2021-06-23 16:22:10 +0100

    add a setting to tell Heroku where the static files are

--- dublinbus/dublinbus/settings.py
+++ dublinbus/dublinbus/settings.py
@@ -11,9 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Quick-start development settings - unsuitable for production
