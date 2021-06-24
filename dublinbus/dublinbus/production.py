c202310c90a15ddcc0b3248e1ade7ca1bfd32bb1
AY <anyi.huang@ucdconnect.ie>
2021-06-24 22:59:49 +0100

    Retrieve environmental setting

--- dublinbus/dublinbus/production.py
+++ dublinbus/dublinbus/production.py
@@ -21,10 +21,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-1)#fo!fd3$fvtg+#7bo-0r_f4#!5_)zp2mtezez&eat@27okx="
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ['DEBUG_VALUE']
 
 ALLOWED_HOSTS = ['dublin-bus-team14.herokuapp.com']
 
