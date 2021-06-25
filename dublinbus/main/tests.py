b7e2c462b11ed23b2f7fbb3d98bef9d3d2f2c9cc
sn-lp <sofia.pereira@ucdconnect.ie>
2021-06-25 15:06:04 +0100

    Remove previous changes to tests.py - fix github secrets

--- dublinbus/main/tests.py
+++ dublinbus/main/tests.py
@@ -1,13 +1,11 @@
-from django.conf import settings
 from django.urls import reverse
 from django.test import TestCase
 
 
 class ViewTests(TestCase):
     def test_index_view(self):
-        with settings.GOOGLE_MAPS_API_KEY:
-            response = self.client.get(reverse("home"))
-            self.assertEqual(response.status_code, 200)
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
 
     def test_real_time_view(self):
         response = self.client.get(reverse("realtime"))
