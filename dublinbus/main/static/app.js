e74fc6fe4062bbb7566043cc098d64fa246128ee
Sofia Pereira <66690399+sn-lp@users.noreply.github.com>
2021-06-25 16:44:41 +0100

    Bootstrap initial (#9)
    * Bootstrap and MDB is now integrated into the project. 'static' folder has been created within the Django folder structure. All CSS and JS files for both Boostrap and MD Bootstrap can be found within this folder. A simple Navbar has been implemented from an MDB template.
    
    * Attempts to implement Google Maps API. Currently not loading.
    
    * Fix map display in all pages, read googlemaps api key from an env variable and add path to prettierignore to not format bootstrap and MDB files
    
    * Fix CI steps to read env var from Github secrets
    
    * Try to fix CI tests for testing app views
    
    * Try fix testing views with env variable
    
    * Remove previous changes to tests.py - fix github secrets
    
    * Try fix CI steps
    
    * Fix CI by naming github secret variable in the correct way
    
    Co-authored-by: kyle-stuart <strtkyl@gmail.com>

---
+++ dublinbus/main/static/app.js
@@ -0,0 +1,8 @@
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 53.3498, lng: -6.2603 },
+    zoom: 12,
+  });
+}
--- dublinbus/main/views.py
+++ dublinbus/main/views.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+
 from django.template import loader
 from django.shortcuts import render
 from django.http import HttpResponse
@@ -5,17 +7,26 @@ from django.http import HttpResponse
 
 def index(request):
     template = loader.get_template("main/index.html")
-    context = {"title": "Dublin Bus Homepage", "team_number": 14}
+    if not settings.GOOGLE_MAPS_API_KEY:
+        # if key is empty because it couldn't find the env variable, return an "Internal Server Error" 500 response code
+        return HttpResponse(status=500)
+    context = {"GOOGLEMAPS_APIKEY": settings.GOOGLE_MAPS_API_KEY}
     return HttpResponse(template.render(context, request))
 
 
 def real_time(request):
     template = loader.get_template("main/realtime.html")
-    context = {"title": "Dublin Bus Real time info page", "team_number": 14}
+    if not settings.GOOGLE_MAPS_API_KEY:
+        # if key is empty because it couldn't find the env variable, return an "Internal Server Error" 500 response code
+        return HttpResponse(status=500)
+    context = {"GOOGLEMAPS_APIKEY": settings.GOOGLE_MAPS_API_KEY}
     return HttpResponse(template.render(context, request))
 
 
 def bus_routes(request):
     template = loader.get_template("main/routes.html")
-    context = {"title": "Dublin Bus routes page", "team_number": 14}
+    if not settings.GOOGLE_MAPS_API_KEY:
+        # if key is empty because it couldn't find the env variable, return an "Internal Server Error" 500 response code
+        return HttpResponse(status=500)
+    context = {"GOOGLEMAPS_APIKEY": settings.GOOGLE_MAPS_API_KEY}
     return HttpResponse(template.render(context, request))
