1b18086d98fe1554a18a3e9e9055f747211af43f
sn-lp <sofia.pereira@ucdconnect.ie>
2021-06-25 15:44:47 +0100

    Fix CI by naming github secret variable in the correct way

--- dublinbus/main/views.py
+++ dublinbus/main/views.py
@@ -7,29 +7,26 @@ from django.http import HttpResponse
 
 def index(request):
     template = loader.get_template("main/index.html")
-    GOOGLE_MAPS_API_KEY = settings.GOOGLE_MAPS_API_KEY
-    if not GOOGLE_MAPS_API_KEY:
+    if not settings.GOOGLE_MAPS_API_KEY:
         # if key is empty because it couldn't find the env variable, return an "Internal Server Error" 500 response code
         return HttpResponse(status=500)
-    context = {"GOOGLEMAPS_APIKEY": GOOGLE_MAPS_API_KEY}
+    context = {"GOOGLEMAPS_APIKEY": settings.GOOGLE_MAPS_API_KEY}
     return HttpResponse(template.render(context, request))
 
 
 def real_time(request):
     template = loader.get_template("main/realtime.html")
-    GOOGLE_MAPS_API_KEY = settings.GOOGLE_MAPS_API_KEY
-    if not GOOGLE_MAPS_API_KEY:
+    if not settings.GOOGLE_MAPS_API_KEY:
         # if key is empty because it couldn't find the env variable, return an "Internal Server Error" 500 response code
         return HttpResponse(status=500)
-    context = {"GOOGLEMAPS_APIKEY": GOOGLE_MAPS_API_KEY}
+    context = {"GOOGLEMAPS_APIKEY": settings.GOOGLE_MAPS_API_KEY}
     return HttpResponse(template.render(context, request))
 
 
 def bus_routes(request):
     template = loader.get_template("main/routes.html")
-    GOOGLE_MAPS_API_KEY = settings.GOOGLE_MAPS_API_KEY
-    if not GOOGLE_MAPS_API_KEY:
+    if not settings.GOOGLE_MAPS_API_KEY:
         # if key is empty because it couldn't find the env variable, return an "Internal Server Error" 500 response code
         return HttpResponse(status=500)
-    context = {"GOOGLEMAPS_APIKEY": GOOGLE_MAPS_API_KEY}
+    context = {"GOOGLEMAPS_APIKEY": settings.GOOGLE_MAPS_API_KEY}
     return HttpResponse(template.render(context, request))
