63f5b6b89b75be82f78581e5dac46ae9104f870c
sn-lp <sofia.pereira@ucdconnect.ie>
2021-06-25 14:54:51 +0100

    Try fix testing views with env variable

--- dublinbus/main/tests.py
+++ dublinbus/main/tests.py
@@ -1,14 +1,13 @@
 from django.conf import settings
-
 from django.urls import reverse
 from django.test import TestCase
 
 
-
 class ViewTests(TestCase):
     def test_index_view(self):
-        response = self.client.get(reverse("home"))
-        self.assertEqual(response.status_code, 200)
+        with settings.GOOGLE_MAPS_API_KEY:
+            response = self.client.get(reverse("home"))
+            self.assertEqual(response.status_code, 200)
 
     def test_real_time_view(self):
         response = self.client.get(reverse("realtime"))
