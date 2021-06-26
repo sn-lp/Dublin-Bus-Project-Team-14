238bdf58fb3a7464e9b50a626a0a6cc90d08d04b
AY <anyi.huang@ucdconnect.ie>
2021-06-26 14:23:03 +0100

    black .

--- dublinbus/dublinbus/settings.py
+++ dublinbus/dublinbus/settings.py
@@ -21,14 +21,14 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-if os.getenv('SECRET_KEY'):
-    SECRET_KEY = os.environ['SECRET_KEY']
+if os.getenv("SECRET_KEY"):
+    SECRET_KEY = os.environ["SECRET_KEY"]
 else:
-    SECRET_KEY = 'django-insecure-1)#fo!fd3$fvtg+#7bo-0r_f4#!5_)zp2mtezez&eat@27okx='
+    SECRET_KEY = "django-insecure-1)#fo!fd3$fvtg+#7bo-0r_f4#!5_)zp2mtezez&eat@27okx="
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if os.getenv('DEBUG_VALUE'):
-    DEBUG = os.environ['DEBUG_VALUE']
+if os.getenv("DEBUG_VALUE"):
+    DEBUG = os.environ["DEBUG_VALUE"]
 else:
     DEBUG = True
 
