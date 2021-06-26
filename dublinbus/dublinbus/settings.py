49bb5321fc229767921e459f67898bcd1f637863
AY <anyi.huang@ucdconnect.ie>
2021-06-26 14:15:35 +0100

    settings for Heroku

--- dublinbus/dublinbus/settings.py
+++ dublinbus/dublinbus/settings.py
@@ -15,18 +15,24 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-1)#fo!fd3$fvtg+#7bo-0r_f4#!5_)zp2mtezez&eat@27okx="
+if os.getenv('SECRET_KEY'):
+    SECRET_KEY = os.environ['SECRET_KEY']
+else:
+    SECRET_KEY = 'django-insecure-1)#fo!fd3$fvtg+#7bo-0r_f4#!5_)zp2mtezez&eat@27okx='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if os.getenv('DEBUG_VALUE'):
+    DEBUG = os.environ['DEBUG_VALUE']
+else:
+    DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["dublin-bus-team14.herokuapp.com"]
 
 
 # Application definition
