98170901223a35cfec4b69439914476e781376f7
kyle-stuart <strtkyl@gmail.com>
2021-07-05 16:01:18 +0100

    slight formatting changes

--- dublinbus/main/static/app.js
+++ dublinbus/main/static/app.js
@@ -10,29 +10,8 @@ function initMap() {
   });
 }
 
-//Setting datetime
-var today = new Date();
-var year = today.getFullYear();
-var month = today.getMonth() + 1;
-var day = today.getDate();
-var hour = today.getHours();
-var minute = today.getMinutes();
 
-const date_vars = [year, month, day, hour, minute];
 
-for (var i = 0; i < date_vars.length; i++) {
-  if (date_vars[i] < 10) {
-    date_vars[i] = "0" + String(date_vars[i]);
-  } else {
-    date_vars[i] = String(date_vars[i]);
-  }
-  console.log(date_vars[i]);
-}
-
-var datetime_string = date_vars[0] + "-" + date_vars[1] + "-" + date_vars[2] + "T" + date_vars[3] + ":" + date_vars[4];
-
-document.getElementById("departure-time").value = datetime_string;
-document.getElementById("departure-time").min = datetime_string;
 
 //Navigation Drawer
 const drawer = mdc.drawer.MDCDrawer.attachTo(
