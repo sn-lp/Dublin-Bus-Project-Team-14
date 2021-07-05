898f9317eb02c684c4faaedc2681ad2338b0c441
kyle-stuart <70216693+kyle-stuart@users.noreply.github.com>
2021-07-05 14:53:28 +0100

    Material design navbar (#14)
    * Added icon to web app
    
    * Removed previous nav-drawer implementation.
    
    * New implementation of nav drawer
    
    * Prettier format
    
    * deleted package json files

--- dublinbus/main/static/app.js
+++ dublinbus/main/static/app.js
@@ -7,10 +7,12 @@ function initMap() {
   });
 }
 
-function openNav() {
-  document.getElementById("mySidenav").style.width = "50%";
-}
-
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-}
+const drawer = mdc.drawer.MDCDrawer.attachTo(
+  document.querySelector(".mdc-drawer")
+);
+const topAppBar = mdc.topAppBar.MDCTopAppBar.attachTo(
+  document.querySelector(".mdc-top-app-bar")
+);
+topAppBar.listen("MDCTopAppBar:nav", () => {
+  drawer.open = !drawer.open;
+});
