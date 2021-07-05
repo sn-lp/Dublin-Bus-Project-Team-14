246a151ae629704b1ffb4210b267e7b90a02cb41
kyle-stuart <strtkyl@gmail.com>
2021-07-05 15:14:41 +0100

    date vairables

--- dublinbus/main/static/app.js
+++ dublinbus/main/static/app.js
@@ -10,6 +10,28 @@ function initMap() {
   });
 }
 
+//Setting datetime
+var today = new Date();
+var year = today.getFullYear();
+var month = today.getMonth() + 1;
+var day = today.getDate();
+var hour = today.getHours();
+var minute = today.getMinutes();
+
+const date_vars = [year, month, day, hour, minute];
+
+for (var i = 0; i < date_vars.length; i++) {
+  if (date_vars[i] < 10) {
+    date_vars[i] = "0" + String(date_vars[i]);
+  } else {
+    date_vars[i] = String(date_vars[i]);
+  }
+  console.log(date_vars[i]);
+}
+
+
+
+//Navigation Drawer
 const drawer = mdc.drawer.MDCDrawer.attachTo(
   document.querySelector(".mdc-drawer")
 );
