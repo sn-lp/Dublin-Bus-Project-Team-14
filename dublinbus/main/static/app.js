b6887ad0fec05fe2d36227ed74d2f0fefc4f04b5
kyle-stuart <70216693+kyle-stuart@users.noreply.github.com>
2021-07-10 12:23:59 +0100

    Journey planner UI. New branch after git problems (#20)

--- dublinbus/main/static/app.js
+++ dublinbus/main/static/app.js
@@ -1,12 +1,4 @@
-let map;
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 53.3498, lng: -6.2603 },
-    zoom: 12,
-  });
-}
-
+//Navigation Drawer
 const drawer = mdc.drawer.MDCDrawer.attachTo(
   document.querySelector(".mdc-drawer")
 );
