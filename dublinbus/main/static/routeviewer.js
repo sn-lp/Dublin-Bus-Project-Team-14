f1fc974728729b1a3a0c9967aecfdeb284290791
sn-lp <sofia.pereira@ucdconnect.ie>
2021-07-12 15:27:24 +0100

    Handle invalid route numbers and fetch errors, and implement 'back to routes' functionality

--- dublinbus/main/static/routeviewer.js
+++ dublinbus/main/static/routeviewer.js
@@ -28,6 +28,14 @@ function injectDirectionNameInButtons() {
   document.getElementById("direction-button-1").innerText = directions[1];
 }
 
+function displayBackToRoutesButton() {
+  document.getElementById("back-to-routes").style.display = "block";
+}
+
+function goToRoutesPage() {
+  window.location.reload();
+}
+
 function changeDirection(directionNumber) {
   // Get the container element
   const btnContainer = document.getElementById("directions-buttons");
@@ -68,8 +76,26 @@ function getBusStopsFromBackend() {
   routeNumber = document.getElementById("bus-route-input").value;
   busRoutesEndpoint = "/api/get_bus_stops/?route_number=" + routeNumber;
   fetch(busRoutesEndpoint)
-    .then((response) => response.json())
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        document.getElementById("user-error-message").innerText =
+          "Something went wrong, please try again.";
+        // status and statusText doesn't work for all browsers but works in Chrome and Safari
+        throw new Error(response.status + ", " + response.statusText);
+      }
+    })
     .then((busRoutes) => {
+      if (
+        Object.keys(busRoutes).length === 0 &&
+        busRoutes.constructor === Object
+      ) {
+        // if the user inputs an invalid route number, display error message
+        document.getElementById("user-error-message").innerText =
+          "Please enter a valid route number";
+        return;
+      }
       mapDirectionsStops = busRoutes;
       index = 0;
       for (const [direction, stops] of Object.entries(busRoutes)) {
@@ -80,8 +106,12 @@ function getBusStopsFromBackend() {
         index += 1;
       }
       hideSubmitForm();
+      displayBackToRoutesButton();
       displayDirectionsButtons();
       injectDirectionNameInButtons();
       selectedDirection = directions[0];
+    })
+    .catch((error) => {
+      console.log(error);
     });
 }
