f05d8820884f083b79c4b496015b980d30bcd7ce
sn-lp <sofia.pereira@ucdconnect.ie>
2021-07-12 16:46:43 +0100

    Show infowindow when marker is clicked and recenter map

--- dublinbus/main/static/routeviewer.js
+++ dublinbus/main/static/routeviewer.js
@@ -3,16 +3,27 @@ let mapDirectionsStops = {};
 let selectedButton;
 let selectedDirection;
 let markers = [];
+let infowindow;
+const defaultMapZoomLevel = 11;
+const defaultMapPositionLat = 53.3498;
+const defaultMapPositionLong = -6.2603;
 
 //Google Maps
 let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 53.3498, lng: -6.2603 },
-    zoom: 12,
+    center: { lat: defaultMapPositionLat, lng: defaultMapPositionLong },
+    zoom: defaultMapZoomLevel,
     disableDefaultUI: true,
   });
+
+  // We have one infowindow that is shared between all stops so that only one can be open at a time
+  // this closes the infoWindow if one exists already, so when changing to another stop an existing infoWindow cannot be carried
+  if (infowindow) {
+    infowindow.close();
+  }
+  infowindow = new google.maps.InfoWindow();
 }
 
 function hideSubmitForm() {
@@ -36,7 +47,15 @@ function goToRoutesPage() {
   window.location.reload();
 }
 
+function resetMapPositionAndZoom() {
+  map.setZoom(defaultMapZoomLevel);
+  map.setCenter(
+    new google.maps.LatLng(defaultMapPositionLat, defaultMapPositionLong)
+  );
+}
+
 function changeDirection(directionNumber) {
+  resetMapPositionAndZoom();
   // Get the container element
   const btnContainer = document.getElementById("directions-buttons");
 
@@ -50,6 +69,7 @@ function changeDirection(directionNumber) {
   );
   selectedButton.className += " active";
   selectedDirection = directions[directionNumber];
+  // draw the markers for the selected direction by the user
   drawMarkers(mapDirectionsStops[selectedDirection]);
 }
 
@@ -61,6 +81,8 @@ function drawMarkers(stops) {
   }
   markers = [];
   for (const [stopName, coordinates] of Object.entries(stops)) {
+    // set the infoWindow content to contain the name of the bus stop
+    let contentString = stopName;
     const newMarker = new google.maps.Marker({
       position: {
         lat: coordinates.latitude,
@@ -69,6 +91,14 @@ function drawMarkers(stops) {
       map,
     });
     markers.push(newMarker);
+    newMarker.addListener("click", () => {
+      // Close any open info window
+      if (infowindow) infowindow.close();
+      infowindow.setContent(contentString);
+      infowindow.open(map, newMarker);
+      // Pan map to the selected marker
+      map.panTo(newMarker.getPosition());
+    });
   }
 }
 
@@ -87,16 +117,18 @@ function getBusStopsFromBackend() {
       }
     })
     .then((busRoutes) => {
+      // if the user inputs an invalid route number, the object returned by the DB will be empty
       if (
         Object.keys(busRoutes).length === 0 &&
         busRoutes.constructor === Object
       ) {
-        // if the user inputs an invalid route number, display error message
+        // display error message if the object is empty
         document.getElementById("user-error-message").innerText =
           "Please enter a valid route number";
         return;
       }
       mapDirectionsStops = busRoutes;
+      // when the user clicks on the "submit" button, draw the markers for the default selected direction
       index = 0;
       for (const [direction, stops] of Object.entries(busRoutes)) {
         directions.push(direction);
