4ac64a0f657c09497130c17ed5ea2536de863772
kyle-stuart <strtkyl@gmail.com>
2021-07-12 14:15:02 +0100

    added realtime.js file

---
+++ dublinbus/main/static/realtime.js
@@ -0,0 +1,10 @@
+//Google Maps
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 53.3498, lng: -6.2603 },
+    zoom: 12,
+    disableDefaultUI: true,
+  });
+}
