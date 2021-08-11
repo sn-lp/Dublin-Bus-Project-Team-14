0d79f7194c92ebb80e1e71bb2799af25f611521a
kyle-stuart <strtkyl@gmail.com>
2021-08-11 14:17:41 +0100

    realtime zoomed out further (improves clustering)

--- dublinbus/main/static/realtime.js
+++ dublinbus/main/static/realtime.js
@@ -4,7 +4,7 @@ let map;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 53.3498, lng: -6.2603 },
-    zoom: 15,
+    zoom: 12,
     disableDefaultUI: true,
   });
   infowindow = new google.maps.InfoWindow();
