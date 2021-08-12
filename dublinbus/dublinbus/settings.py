85576c0beb5c6a0352a3d0b107ac328c56b058d5
An-Yi-Huang <anyi.huang@ucdconnect.ie>
2021-08-12 12:39:22 +0100

    black .

--- dublinbus/dublinbus/settings.py
+++ dublinbus/dublinbus/settings.py
@@ -187,7 +187,7 @@ CACHES = {
     }
 }
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID", "")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY", "")
 AWS_STORAGE_BUCKET_NAME = os.getenv("S3_BUCKET_NAME", "")
