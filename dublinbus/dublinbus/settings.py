ae5bce1c4a911cd17afacb93e2835127c71e2db0
An-Yi-Huang <71880332+An-Yi-Huang@users.noreply.github.com>
2021-08-13 01:49:34 +0100

    Setup file server for machine learning models (#64)
    * Update requirements.txt
    
    * Update settings.py
    
    * Update tests.py
    
    * Update api.py
    
    * Update requirements.txt
    
    * black .
    
    * updates for CI

--- dublinbus/dublinbus/settings.py
+++ dublinbus/dublinbus/settings.py
@@ -186,3 +186,8 @@ CACHES = {
         "LOCATION": os.path.join(BASE_DIR, "django_cache"),
     }
 }
+
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID", "")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY", "")
+AWS_STORAGE_BUCKET_NAME = os.getenv("S3_BUCKET_NAME", "")
